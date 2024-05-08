6a09d67e2f66798eab4f08dcddc8dda9100e45de
motyliar <j.turostowski@gmail.com>
2024-05-08 11:54:41 +0200

    update start usecase

--- lib/domain/usecase/start_usecase.dart
+++ lib/domain/usecase/start_usecase.dart
@@ -1,11 +1,11 @@
-import 'package:metronome/domain/entity/metronome_tick.dart';
+import 'package:metronome/domain/metronome/metronome_tick.dart';
 import 'package:metronome/domain/inputs/metronome_d.dart';
 
 class StartTimerUsecase {
   const StartTimerUsecase({required MetronomeTick tick}) : _tick = tick;
   final MetronomeTick _tick;
 
-  void execute(MetronomeInputs inputs) {
+  int execute(MetronomeInputs inputs) {
     return _tick.start(inputs);
   }
 }
