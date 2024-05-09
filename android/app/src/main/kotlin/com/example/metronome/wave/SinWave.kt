f1645f38ab503638b679fef0bcc6e4e3928b2525
motyliar <j.turostowski@gmail.com>
2024-05-09 11:53:17 +0200

    create sin wave

--- android/app/src/main/kotlin/com/example/metronome/wave/SinWave.kt
+++ android/app/src/main/kotlin/com/example/metronome/wave/SinWave.kt
@@ -1,4 +1,21 @@
 package com.example.metronome.wave
 
-class SinWave {
+import kotlin.math.sin
+
+class SinWave(frequency: Double, time: Int) : TableWaveGenerator(frequency, time) {
+
+    override fun generateSound(): ShortArray {
+            val numSamples = timeCalculator()
+            var wave = ShortArray(numSamples)
+
+            for(i in 0 until numSamples) {
+                val sample = (amplitude * sin(2 * Math.PI * frequency * i) / sampleRate).toInt()
+                wave[i] = sample.toShort()
+            }
+        return wave
+
+
+    }
+
+
 }
