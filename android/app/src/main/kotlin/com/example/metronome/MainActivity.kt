38e9a2223867e07a69a536da4c24bb93126d36f2
motyliar <j.turostowski@gmail.com>
2024-05-09 13:04:27 +0200

    create method channel

--- android/app/src/main/kotlin/com/example/metronome/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/metronome/MainActivity.kt
@@ -5,24 +5,41 @@ import android.media.AudioFormat
 import android.media.AudioManager
 import android.media.AudioTrack
 import android.os.Build
+import com.example.metronome.player.MetronomePlayer
+import com.example.metronome.sound.SoundShifter
+import com.example.metronome.wave.WaveFactoryBuffer
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    val channel = "metronome"
+    val metronome: MetronomePlayer = MetronomePlayer()
+    val shifter: SoundShifter = SoundShifter()
+
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, channel)
+            .setMethodCallHandler {
+                call, result ->
+                when(call.method) {
+                    MethodChannelNames.INIT.name.lowercase() -> WaveFactoryBuffer.initialize()
+                    MethodChannelNames.PLAY.name.lowercase() -> {
+                        val audio = call.argument<String>("audio")
+                        val accent = call.argument<String>("accent")
+                        val wave = WaveFactoryBuffer.getCurrentWave(audio!!)
+                        val waveTable = shifter.set(wave, accent!!)
+
+
+                        metronome.play(waveTable)}
+                }
+            }
     }
 
-   lateinit var audioTrack: AudioTrack
-   private val sampleRate = 44000
+}
 
-   fun createPlayer(context: Context) {
-       audioTrack = AudioTrack(AudioManager.STREAM_MUSIC, sampleRate, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT, sampleRate, AudioTrack.MODE_STREAM)
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            audioTrack.setVolume(1.0F)
-        }
-       audioTrack.play()
-   }
 
-    
+enum class MethodChannelNames {
+    INIT, PLAY
 }
