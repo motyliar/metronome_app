00aab19f82fed860ad54229cf12b5d98f94e45d3
motyliar <j.turostowski@gmail.com>
2024-05-09 11:53:29 +0200

    create square wave

--- android/app/src/main/kotlin/com/example/metronome/wave/SquareWave.kt
+++ android/app/src/main/kotlin/com/example/metronome/wave/SquareWave.kt
@@ -1,4 +1,18 @@
 package com.example.metronome.wave
 
-class SquareWave {
+import kotlin.math.sign
+import kotlin.math.sin
+
+class SquareWave(frequency: Double, time: Int) : TableWaveGenerator(frequency, time) {
+
+    override fun generateSound(): ShortArray {
+        val numSample = timeCalculator()
+        val wave = ShortArray(numSample)
+
+        for(i in 0 until numSample) {
+            val sample = (amplitude * sign(sin((Math.PI * 2 * frequency) * i / sampleRate)) ) .toInt()
+            wave[i] = sample.toShort()
+        }
+        return wave
+    }
 }
