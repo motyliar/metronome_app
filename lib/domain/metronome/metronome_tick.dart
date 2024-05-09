78d78903a82e0d1db6d65e2dbf69f1ecd271f5be
motyliar <j.turostowski@gmail.com>
2024-05-09 08:40:28 +0200

    update start function to output stream<int>

--- lib/domain/metronome/metronome_tick.dart
+++ lib/domain/metronome/metronome_tick.dart
@@ -1,7 +1,9 @@
+import 'dart:async';
+
 import 'package:metronome/domain/inputs/metronome_d.dart';
 
 abstract class MetronomeTick {
-  int start(MetronomeInputs inputs);
+  Stream<int> start(MetronomeInputs inputs);
   void stop();
   void pause();
   void resume();
