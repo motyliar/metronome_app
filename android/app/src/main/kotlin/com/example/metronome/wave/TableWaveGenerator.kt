8947c38922e79619b29e623d2dba3d56cbed90ce
motyliar <j.turostowski@gmail.com>
2024-05-09 11:52:55 +0200

    create abs class TableWaveGenerator

--- android/app/src/main/kotlin/com/example/metronome/wave/TableWaveGenerator.kt
+++ android/app/src/main/kotlin/com/example/metronome/wave/TableWaveGenerator.kt
@@ -1,4 +1,19 @@
 package com.example.metronome.wave
 
-class TableWaveGenerator {
+abstract class TableWaveGenerator(frequency: Double, time: Int) {
+
+
+
+    protected val frequency = frequency
+    protected val time = time
+    protected val sampleRate = 44100
+    protected val amplitude = Short.MAX_VALUE
+     abstract fun generateSound(): ShortArray
+
+    protected fun timeCalculator(): Int {
+        return sampleRate / time
+
+    }
 }
+
+
