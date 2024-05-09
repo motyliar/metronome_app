cd32e7432c9ebb4348f15ab93dc165e811c77219
motyliar <j.turostowski@gmail.com>
2024-05-09 08:41:10 +0200

    update output of execute function

--- lib/domain/usecase/start_usecase.dart
+++ lib/domain/usecase/start_usecase.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:metronome/domain/metronome/metronome_tick.dart';
 import 'package:metronome/domain/inputs/metronome_d.dart';
 
@@ -5,7 +7,7 @@ class StartTimerUsecase {
   const StartTimerUsecase({required MetronomeTick tick}) : _tick = tick;
   final MetronomeTick _tick;
 
-  int execute(MetronomeInputs inputs) {
+  Stream<int> execute(MetronomeInputs inputs) {
     return _tick.start(inputs);
   }
 }
