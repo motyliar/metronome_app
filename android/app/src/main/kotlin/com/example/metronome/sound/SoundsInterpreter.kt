7d8e78030d056aae6a630693020110c9b4359e19
motyliar <j.turostowski@gmail.com>
2024-05-10 11:14:53 +0200

    create class to interpret message from flutter

--- android/app/src/main/kotlin/com/example/metronome/sound/SoundsInterpreter.kt
+++ android/app/src/main/kotlin/com/example/metronome/sound/SoundsInterpreter.kt
@@ -1,4 +1,15 @@
 package com.example.metronome.sound
 
-class SoundsInterpreter {
+import com.example.metronome.wave.WaveFactoryBuffer
+
+
+
+class SoundsInterpreter(private val sounds: List<ShortArray>) {
+    fun interpret(pitch: String, asset: String): ShortArray{
+        return when(asset) {
+            "tambourine" -> if(pitch == "high") { return sounds[0]
+            } else { return sounds[1]
+            } else -> WaveFactoryBuffer.getSound(asset, pitch)
+        }
+    }
 }
