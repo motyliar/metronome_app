68673d4f38566e5ac29c6b02c0b8cd4f52766317
motyliar <j.turostowski@gmail.com>
2024-05-10 11:58:23 +0200

    add dependency of tempocalculator usecase

--- lib/presentation/metronome/business/metronome/metronome_cubit.dart
+++ lib/presentation/metronome/business/metronome/metronome_cubit.dart
@@ -7,6 +7,7 @@ import 'package:metronome/domain/inputs/audio_inputs.dart';
 import 'package:metronome/domain/inputs/metronome_d.dart';
 import 'package:metronome/domain/metronome/accent_handler.dart';
 import 'package:metronome/domain/metronome/audio_asset.dart';
+import 'package:metronome/domain/usecase/calculate_tempo_usecase.dart';
 
 import 'package:metronome/domain/usecase/send_message_usecase.dart';
 import 'package:metronome/domain/usecase/start_usecase.dart';
@@ -18,18 +19,21 @@ class MetronomeCubit extends Cubit<MetronomeState> {
   final StartTimerUsecase _start;
   final StopPlayerUsecase _stop;
   final ConnectUsecase _send;
+  final CalculateTempoUsecase _calculate;
   MetronomeCubit({
     required StartTimerUsecase start,
     required StopPlayerUsecase stop,
     required ConnectUsecase send,
+    required CalculateTempoUsecase calculate,
   })  : _start = start,
         _stop = stop,
         _send = send,
+        _calculate = calculate,
         super(MetronomeState(accents: AccentHandler()));
   StreamSubscription? sub;
   void sendMessageToNative() {
     sub = _start
-        .execute(const MetronomeInputs(durationInMilliseconds: 400))
+        .execute(const MetronomeInputs(durationInMilliseconds: 180))
         .listen((event) {
       emit(MetronomeState(
           metrum: (event % state.tick) + 1,
