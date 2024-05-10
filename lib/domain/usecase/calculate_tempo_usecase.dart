5113db092ac0f14604fc31423512a9d756c4cd04
motyliar <j.turostowski@gmail.com>
2024-05-10 11:49:44 +0200

    create usecase to implementation tempo calculator

---
+++ lib/domain/usecase/calculate_tempo_usecase.dart
@@ -0,0 +1,13 @@
+import 'package:metronome/domain/metronome/manual_tempo_calculator.dart';
+
+class CalculateTempoUsecase extends ManualTempoCalculator {
+  @override
+  int onSet() {
+    throw UnimplementedError();
+  }
+
+  @override
+  int onStop() {
+    throw UnimplementedError();
+  }
+}
