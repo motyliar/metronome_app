5b2dfcb45cba62631905b18bcb32b40e354bf77d
motyliar <j.turostowski@gmail.com>
2024-05-10 09:07:11 +0200

    update method channel

--- android/app/src/main/kotlin/com/example/metronome/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/metronome/MainActivity.kt
@@ -7,32 +7,40 @@ import android.media.AudioTrack
 import android.os.Build
 import com.example.metronome.player.MetronomePlayer
 import com.example.metronome.sound.SoundShifter
+import com.example.metronome.wave.SinWave
 import com.example.metronome.wave.WaveFactoryBuffer
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
+import kotlin.math.sin
 
 class MainActivity: FlutterActivity() {
-    val channel = "metronome"
-    val metronome: MetronomePlayer = MetronomePlayer()
-    val shifter: SoundShifter = SoundShifter()
+   private val channel = "metronome"
+   private val metronome: MetronomePlayer = MetronomePlayer()
+   private val shifter: SoundShifter = SoundShifter()
+
+
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
+        metronome.initPlayer()
+        metronome.audioTrack?.play()
+        WaveFactoryBuffer.initialize()
 
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, channel)
             .setMethodCallHandler {
                 call, result ->
                 when(call.method) {
-                    MethodChannelNames.INIT.name.lowercase() -> WaveFactoryBuffer.initialize()
-                    MethodChannelNames.PLAY.name.lowercase() -> {
+                    MethodChannelNames.INIT.name -> WaveFactoryBuffer.initialize()
+                    MethodChannelNames.PLAY.name -> {
                         val audio = call.argument<String>("audio")
+                        println("asset: $audio")
                         val accent = call.argument<String>("accent")
-                        val wave = WaveFactoryBuffer.getCurrentWave(audio!!)
-                        val waveTable = shifter.set(wave, accent!!)
+                        val sound = WaveFactoryBuffer.getCurrentWave(audio!!)
+                        val wave = shifter.set(sound, accent!!)
 
-
-                        metronome.play(waveTable)}
+                        metronome.play(wave)
+                    }
                 }
             }
     }
@@ -43,3 +51,6 @@ class MainActivity: FlutterActivity() {
 enum class MethodChannelNames {
     INIT, PLAY
 }
+
+
+
