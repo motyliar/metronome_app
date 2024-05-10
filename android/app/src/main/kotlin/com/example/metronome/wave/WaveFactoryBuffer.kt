68a58c39f70b52335f586d248d6267695dcf650a
motyliar <j.turostowski@gmail.com>
2024-05-10 11:18:35 +0200

    add new function

--- android/app/src/main/kotlin/com/example/metronome/wave/WaveFactoryBuffer.kt
+++ android/app/src/main/kotlin/com/example/metronome/wave/WaveFactoryBuffer.kt
@@ -1,26 +1,45 @@
 package com.example.metronome.wave
 
+import java.lang.UnsupportedOperationException
+
 enum class WaveFactoryBuffer(val high: ShortArray,val mid: ShortArray,val low: ShortArray) {
     SIN(
         high = HighSinWave.generateSound(), mid = MidSinWave.generateSound(), low = LowSinWave.generateSound()
     ), SQUARE(
-        high = HighSquareWave.generateSound(), mid = MidSquareWave.generateSound(), low = LowSqureWave.generateSound()
-    );
+        high = HighSquareWave.generateSound(), mid = MidSquareWave.generateSound(), low = LowSquareWave.generateSound()
+    ),
+    ;
 
     companion object {
         fun initialize() {
             values()
         }
 
-        fun getCurrentWave(name: String): WaveFactoryBuffer {
+
+
+        private fun getCurrentWave(name: String): WaveFactoryBuffer {
             return when(name) {
                 "sin" -> SIN
                 "square" -> SQUARE
                 else -> SIN
             }
         }
+
+        fun getSound(tableWave: String, pitch: String): ShortArray {
+            val wave = getCurrentWave(tableWave)
+            return when(pitch) {
+                "high" -> wave.high
+                "mid" -> wave.mid
+                "low" -> wave.low
+                else -> throw UnsupportedOperationException()
+            }
+
+        }
     }
 
 
 
 }
+
+
+
